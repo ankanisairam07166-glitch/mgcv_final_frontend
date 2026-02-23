d226825cbcbe61f35771316931dd1f9e8f58cfa8
sairam <ankanisairam07166@gmail.com>
2026-02-23 17:53:21 +0530

    Update axiosConfig.ts

--- src/services/api/axiosConfig.ts
+++ src/services/api/axiosConfig.ts
@@ -21,7 +21,7 @@
 // export default axiosInstance
 import axios from 'axios'
 
-export const BACKEND_URL = process.env.NEXT_PUBLIC_API_BASE_URL || 'http://13.235.33.191'
+export const BACKEND_URL = process.env.NEXT_PUBLIC_API_BASE_URL || ""
 
 const axiosInstance = axios.create({
   baseURL: BACKEND_URL,
