931fba0b1b74dc7c746b7278c2f4d3a266869dbd
sairam <ankanisairam07166@gmail.com>
2026-02-23 15:54:45 +0530

    Update axiosConfig.ts

--- src/services/api/axiosConfig.ts
+++ src/services/api/axiosConfig.ts
@@ -1,10 +1,30 @@
 
+// import axios from 'axios'
+
+// export const BACKEND_URL = process.env.NEXT_PUBLIC_API_URL || ""
+
+// const axiosInstance = axios.create({
+//   baseURL: `${BACKEND_URL}/`,
+//   headers: {
+//     'Content-Type': 'application/json',
+//   },
+// })
+
+// axiosInstance.interceptors.response.use(
+//   (response) => response,
+//   (error) => {
+//     console.error('API Error:', error)
+//     return Promise.reject(error)
+//   }
+// )
+
+// export default axiosInstance
 import axios from 'axios'
 
-export const BACKEND_URL = process.env.NEXT_PUBLIC_API_URL || ""
+export const BACKEND_URL = process.env.NEXT_PUBLIC_API_BASE_URL || 'http://13.235.33.191'
 
 const axiosInstance = axios.create({
-  baseURL: `${BACKEND_URL}/`,
+  baseURL: BACKEND_URL,
   headers: {
     'Content-Type': 'application/json',
   },
