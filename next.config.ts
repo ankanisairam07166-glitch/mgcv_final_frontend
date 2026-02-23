0ef841898555dded354683e2b68f9d520e0ed419
sairam <ankanisairam07166@gmail.com>
2026-02-23 15:16:25 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,26 @@
-import type { NextConfig } from "next";
+// import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
+
+// export default nextConfig;
+
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://13.235.33.191/:path*',
+      },
+    ];
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
+```
 
+Then update env variables to:
+```
+NEXT_PUBLIC_API_BASE_URL = 
+NEXT_PUBLIC_API_URL =
