d7137de76ac8ab1de2fe822a59ff9573c6524630
sairam <ankanisairam07166@gmail.com>
2026-03-05 18:11:28 +0530

    Update axiosConfig.ts

--- src/services/api/axiosConfig.ts
+++ src/services/api/axiosConfig.ts
@@ -21,7 +21,7 @@
 // export default axiosInstance
 import axios from 'axios'
 
-export const BACKEND_URL = process.env.NEXT_PUBLIC_API_URL || "https://racing-witch-chairs-married.trycloudflare.com"
+export const BACKEND_URL = process.env.NEXT_PUBLIC_API_URL || "https://shirts-dining-commander-reading.trycloudflare.com"
 
 const axiosInstance = axios.create({
   baseURL: `${BACKEND_URL}/`,
