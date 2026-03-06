ef8ad3198f7e99c7009493e1acbcde89d57b235f
sairam <ankanisairam07166@gmail.com>
2026-03-06 17:00:55 +0530

    Update url change

--- src/services/api/axiosConfig.ts
+++ src/services/api/axiosConfig.ts
@@ -21,7 +21,7 @@
 // export default axiosInstance
 import axios from 'axios'
 
-export const BACKEND_URL = process.env.NEXT_PUBLIC_API_URL || "https://shirts-dining-commander-reading.trycloudflare.com"
+export const BACKEND_URL = process.env.NEXT_PUBLIC_API_URL || "http://3.6.89.99"
 
 const axiosInstance = axios.create({
   baseURL: `${BACKEND_URL}/`,
